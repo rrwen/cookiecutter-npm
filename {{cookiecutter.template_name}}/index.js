deef3b0c65ac78b073e614527da7b5c6db31fbaf
rwenite <rwenite@gmail.com>
2017-11-17 02:59:09 -0500

    Add github url to docs

--- {{cookiecutter.template_name}}/index.js
+++ {{cookiecutter.template_name}}/index.js
@@ -3,9 +3,13 @@
 
 /**
  * Description.
+ * 
+ * * {@link {{cookiecutter.github_url}} Github Repository}
+ *
  * @module {{cookiecutter.template_name}}
  * @param {Object} [options={}] parameter description.
  * @returns {Object} return description.
+ *
  * @example
  * var {{cookiecutter.template_name.replace('-', '').replace(' ', '')}} = require('../index.js');
  */
