c100057f831f9212a91b02e4ea7df5c5574639ed
rwenite <rwenite@gmail.com>
2017-11-17 01:59:09 -0500

    Add docs

--- {{cookiecutter.template_name}}/index.js
+++ {{cookiecutter.template_name}}/index.js
@@ -1,6 +1,11 @@
 // {{cookiecutter.author}}
 // {{cookiecutter.email}}
 
+/**
+ * Description.
+ * @param {Object} [options={}] parameter description
+ * @returns {Object} return description.
+ */
 module.exports = options => {
 	options = options || {};
 };
