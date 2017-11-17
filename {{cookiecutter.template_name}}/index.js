b59a133f271a7682cd3aa9fcf9d3d03127a248bb
rwenite <rwenite@gmail.com>
2017-11-17 02:18:49 -0500

    Add doc example

--- {{cookiecutter.template_name}}/index.js
+++ {{cookiecutter.template_name}}/index.js
@@ -3,8 +3,11 @@
 
 /**
  * Description.
- * @param {Object} [options={}] parameter description
+ * @module {{cookiecutter.template_name}}
+ * @param {Object} [options={}] parameter description.
  * @returns {Object} return description.
+ * @example
+ * var {{cookiecutter.template_name.replace('-', '').replace(' ', '')}} = require('../index.js');
  */
 module.exports = options => {
 	options = options || {};
